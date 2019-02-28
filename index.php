e18215833a022026116b9be04789bb2218c13f85
parvinderiapp <parvinder.s@iapptechnologies.com>
2019-02-28 16:48:37 +0530

    mongo

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
-	echo "winter was good";
-	echo "<br>"."sfdsf";
+	<?php
+  # get the mongo db name out of the env
+ echo $mongo_url = parse_url(getenv("MONGO_URL"));
+
+
+ die;
+  $dbname = str_replace("/", "", $mongo_url["path"]);
+
+  # connect
+  $m   = new Mongo(getenv("MONGO_URL"));
+  $db  = $m->$dbname;
+  $col = $db->access;
+
+  # insert a document
+  $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
+  $col->insert($visit);
+
+  # print all existing documents
+  $data = $col->find();
+  foreach($data as $visit) {
+    echo "<li>" . $visit["ip"] . "</li>";
+  }
+
+  # disconnect
+  $m->close();
+?>
+
+
 ?>
